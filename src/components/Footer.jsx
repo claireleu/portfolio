b77d680c405f38ec1cf9bf7eb49e8cc50c132a98
claireleu <claireliu6829@gmail.com>
2026-01-07 02:10:17 -0500

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer () {
         <div className="w-full flex flex-col gap-[10px] px-[70px] py-[7px] font-effra font-light text-light-grey">
             <div className="w-full h-[1px] bg-light-grey rounded-full"></div>
             <div className="flex items-center justify-between leading-[0.9]">
-                <span>© 2026 Claire Liu</span>
+                <span>Made byClaire Liu © 2026</span>
                 <SocialLinks className="text-light-grey"/>
             </div>
         </div>
